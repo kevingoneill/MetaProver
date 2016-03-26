1227a5440522239db4d0135e938a1202803c978a
Mitchell Mellone <mellom3@rpi.edu>
2016-03-26 00:05:29 -0400

    Implemented IS

--- src/expression/metasentence/IS.java
+++ src/expression/metasentence/IS.java
@@ -1,6 +1,7 @@
 package expression.metasentence;
 
 import expression.sentence.Sentence;
+import logicalreasoner.truthassignment.TruthAssignment;
 import metareasoner.metainference.MetaInference;
 import metareasoner.proof.Proof;
 
@@ -13,15 +14,26 @@ import java.util.HashSet;
  */
 public class IS extends MetaSentence {
 
+	private Boolean value;
+	
     public IS(Sentence s, MetaConstant c) {
         super(new ArrayList<>(Arrays.asList(s, c)), "IS", "is a", new HashSet<>());
+        value = c.getValue();
     }
 
     public MetaInference reasonForwards(Proof p, int inferenceNum) {
-        return null;
+        return reason(p, inferenceNum);
     }
 
     public MetaInference reasonBackwards(Proof p, int inferenceNum) {
-        return null;
+        return reason(p, inferenceNum);
+    }
+    
+    public MetaInference reason(Proof p, int inferenceNum) {
+    	TruthAssignmentVar t = new TruthAssignmentVar(new TruthAssignment());
+    	MODELS m = new MODELS(t, (Sentence)args.get(0), value, inferenceNum);
+    	ArrayList<MetaSentence> a = new ArrayList<>();
+        a.add(m);
+    	return new MetaInference(this, a, inferenceNum);
     }
 }
--- src/expression/metasentence/MetaConstant.java
+++ src/expression/metasentence/MetaConstant.java
@@ -10,17 +10,21 @@ import java.util.HashSet;
  * Created by kevin on 3/22/16.
  */
 public class MetaConstant extends MetaSentence {
-    public static final MetaConstant TAUTOLOGY = new MetaConstant(true),
-            CONTRADICTION = new MetaConstant(false),
-        CONTINGENCY = new MetaConstant(null);
+    public static final MetaConstant TAUTOLOGY = new MetaConstant(true, "Tautology"),
+            CONTRADICTION = new MetaConstant(false, "Contradiction"),
+        CONTINGENCY = new MetaConstant(null, "Contingency");
 
     private Boolean value;
 
-    private MetaConstant(Boolean b) {
-        super(new ArrayList<>(), Boolean.toString(b), Boolean.toString(b), new HashSet<>());
+    private MetaConstant(Boolean b, String name) {
+        super(new ArrayList<>(), name, name, new HashSet<>());
         value = b;
     }
 
+    public Boolean getValue() {
+    	return value;
+    }
+    
     public MetaInference reasonForwards(Proof p, int inferenceNum) {
         return null;
     }
