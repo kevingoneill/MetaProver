565e9df161116d32f6e9e6524f884d010c85e1ad
kevingoneill <oneilk4@rpi.edu>
2016-07-28 11:35:44 -0400

    Created a function for declaring function symbols and their Sorts

--- src/expression/sentence/Function.java
+++ src/expression/sentence/Function.java
@@ -6,11 +6,48 @@ import logicalreasoner.truthassignment.TruthAssignment;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by kevin on 7/28/16.
  */
 public class Function extends Sentence {
+
+  static Map<String, ArrayList<Sort>> functionDeclarations = new HashMap<>();
+
+  /**
+   * Create a declaration for a function named s, taking arguments of argTypes arnd returning a returnType
+   *
+   * @param s          the name of the new Function declaration
+   * @param returnType the return type of the Function being declared
+   * @param argTypes   the argument types of the Function being declared
+   * @return true if the declaration was successful, false otherwise
+   */
+  public static boolean addDeclaration(String s, Sort returnType, ArrayList<Sort> argTypes) {
+    if (functionDeclarations.containsKey(s))
+      return false;
+    argTypes.add(0, returnType);
+    functionDeclarations.put(s, argTypes);
+    return true;
+  }
+
+  /**
+   * Create a declaration for a function named s, taking arguments of argTypes arnd returning a returnType
+   *
+   * @param s          the name of the new Function declaration
+   * @param returnType the return type of the Function being declared
+   * @param argTypes   the argument types of the Function being declared
+   * @return true if the declaration was successful, false otherwise
+   */
+  public static boolean addDeclaration(String s, Sort returnType, Sort... argTypes) {
+    if (functionDeclarations.containsKey(s))
+      return false;
+    ArrayList<Sort> a = new ArrayList<>(Arrays.asList(argTypes));
+    a.add(0, returnType);
+    functionDeclarations.put(s, a);
+    return true;
+  }
 
   public Function(String n, Sort sort, Sentence... sentences) {
     super(new ArrayList<>(Arrays.asList(sentences)), n, n, sort);
